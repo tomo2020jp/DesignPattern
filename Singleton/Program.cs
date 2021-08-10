571c7be458291984abf8849015e7370c5ba2aa31
bruno1911 <39954318+bruno1911@users.noreply.github.com>
2021-08-10 10:45:12 +0900

    Singletonパターン追加

---
+++ Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start.");
+            var obj1 = Singleton.GetSingleton();
+            var obj2 = Singleton.GetSingleton();
+            if (obj1 == obj2)
+            {
+                Console.WriteLine("obj1とobj2は同じインスタンスです。");
+            }
+            else
+            {
+                Console.WriteLine("obj1とobj2は同じインスタンスではありません。");
+            }
+            Console.WriteLine("End.");
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton singleton = new Singleton();
+        private Singleton()
+        {
+            Console.WriteLine("インスタンスを生成しました。");
+        }
+
+        public static Singleton GetSingleton()
+        {
+            return singleton;
+        }
+    }
+}
